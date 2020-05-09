51ed09d248da0067be275c93b1962d2accccae18
kawadumax <kazmax7@gmail.com>
2020-05-09 11:00:35 +0900

    WIP

--- sketch_01_ramdom_walk/src/main.rs
+++ sketch_01_ramdom_walk/src/main.rs
@@ -1,9 +1,10 @@
 use nannou::prelude::*;
 
 fn main() {
+
     nannou::app(model)
         .update(update)
-        .simple_window(view)
+        .view(view)
         .run();
 }
 
@@ -36,7 +37,11 @@ struct Model {
 }
 
 fn model(_app: &App) -> Model {
-    
+    std::fs::remove_dir_all(_app.project_path()
+        .expect("failed to locate `project_path`")
+        .join("./cap/")
+        .join(_app.exe_name().unwrap())).unwrap();
+    _app.new_window().size(100, 100).build().unwrap();
     Model {
         w: Walker {
             x:0,
@@ -50,6 +55,26 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
 fn view(_app: &App, _model: &Model, frame: Frame){
-    // frame.clear(BLACK);
+    if _app.elapsed_frames() == 1 { frame.clear(BLACK) };
     _model.w.display(_app, &frame);
+
+    // Capture the frame!
+    //
+    // NOTE: You can speed this up with `capture_frame_threaded`, however be aware that if the
+    // image writing threads can't keep up you may quickly begin to run out of RAM!
+    let file_path = captured_frame_path(_app, &frame);
+    _app.main_window().capture_frame(file_path);
+}
+
+fn captured_frame_path(_app: &App, frame: &Frame) -> std::path::PathBuf {
+    // Create a path that we want to save this frame to.
+    _app.project_path()
+        .expect("failed to locate `project_path`")
+        .join("./cap/")
+        // Capture all frames to a directory called `/<path_to_nannou>/nannou/simple_capture`.
+        .join(_app.exe_name().unwrap())
+        // Name each file after the number of the frame.
+        .join(format!("{:04}", frame.nth()))
+        // The extension will be PNG. We also support tiff, bmp, gif, jpeg, webp and some others.
+        .with_extension("png")
 }
