1ff6447f3380babd65e5ce20758856607e24bd6c
kawadumax <kazmax7@gmail.com>
2020-09-16 11:54:06 +0900

    scale適用

--- sketch_03_wing/src/particle.rs
+++ sketch_03_wing/src/particle.rs
@@ -1,5 +1,6 @@
 
 use nannou::prelude::*;
+// use nannou::math;
 use crate::WINDOW_WIDTH;
 
 // A simple particle type
@@ -10,6 +11,7 @@ pub struct Particle {
   life_span: f32,
   hue: f32,
   age: f32,
+  scale: f32
 }
 
 impl Particle {
@@ -17,16 +19,19 @@ impl Particle {
     let acceleration = vec2(0.0, 0.02);
     let velocity = vec2(Self::random_f32_width(2.0), random_f32() - 1.0);
     let position = l + vec2(Self::random_f32_width(WINDOW_WIDTH as f32), 0.0);
-    let life_span = 500.0;
+    let life_span = 400.0;
     let hue = random_f32() * 20.0;
     let age = 0.0;
+    let scale = 1.0;
+
     Particle {
       acceleration,
       velocity,
       position,
       life_span,
       hue,
-      age
+      age,
+      scale
     }
   }
 
@@ -36,16 +41,19 @@ impl Particle {
     self.position -= self.velocity / 2.0;
     self.life_span -= 1.0;
     self.age += 0.1;
+    self.scale = 1.0 + pow(self.age, 2) / 200.0;
   }
 
   // Method to display
   pub fn display(&self, draw: &Draw) {
     let size = 28.0;
+    let scale = self.scale;
     draw
+      .scale(scale)
       .ellipse()
-      .xy(self.position)
+      .xy(self.position/scale)
       .w_h(size, size)
-      .hsla(self.hue, 0.8, 0.8, self.life_span / 255.0);
+      .hsl(self.hue, 0.8, 0.8);
 
     self.draw_wing(draw);
     
@@ -62,21 +70,25 @@ impl Particle {
   }
 
   fn draw_wing(&self, draw: &Draw){
-    let radius = 50.0;
-    // let angle = (30.0 % self.age) * PI / 360.0 - PI/6.0;
+    // let scale = 1.0 + self.age/10.0;
+    let radius = 50.0 * self.scale;
+    let weight = 3.0 * self.scale;
     let angle = PI / 6.0 * self.age.cos();
+
     draw
+      // .scale(scale)
       .line()
-      .stroke_weight(3.0)
+      .stroke_weight(weight)
       .start(self.position)
       .end(self.position + vec2(radius * angle.cos(), radius * angle.sin()))
-      .hsla(self.hue, 0.8, 0.8, self.life_span / 255.0);
+      .hsl(self.hue, 0.8, 0.8);
     draw
+      // .scale(scale)
       .line()
-      .stroke_weight(3.0)
+      .stroke_weight(weight)
       .start(self.position)
       .end(self.position + vec2(-radius * angle.cos(), radius * angle.sin()))
-      .hsla(self.hue, 0.8, 0.8, self.life_span / 255.0);
+      .hsl(self.hue, 0.8, 0.8);
   }
 }
 
